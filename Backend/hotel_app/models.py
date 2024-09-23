5797199e9b8789b35abbd86f349e5ecb9279e783
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-09-23 23:32:15 +0500

    Changes

--- Backend/hotel_app/models.py
+++ Backend/hotel_app/models.py
@@ -44,6 +44,8 @@ class Room(models.Model):
     price_per_night = models.DecimalField(max_digits=6, decimal_places=2)
     is_available = models.BooleanField(default=True)
     capacity = models.IntegerField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE,related_name='room_created_by', null=True, blank=True)
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='room_updated_by', null=True, blank=True)
 
     def __str__(self):
         return f"Room {self.room_number} - {self.category}"
