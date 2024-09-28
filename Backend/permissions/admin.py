dff7e1d2fbc1671cf3121742a402c3acd4f6af66
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-09-28 18:43:50 +0500

    Changes

--- Backend/permissions/admin.py
+++ Backend/permissions/admin.py
@@ -3,13 +3,13 @@ from .models import Role, Permission
 
 # Custom admin for Role
 class RoleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code')
+    list_display = ('name', 'code', 'description')
     search_fields = ('name', 'code')
     filter_horizontal = ('permissions',)  # To display many-to-many fields
 
 # Custom admin for Permission
 class PermissionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'module_name')
+    list_display = ('name', 'code', 'module_name', 'description')
     search_fields = ('name', 'code')
 
 # Register models in admin
