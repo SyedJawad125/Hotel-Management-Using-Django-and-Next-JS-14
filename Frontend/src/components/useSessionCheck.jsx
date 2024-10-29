9525c184e37c9f675abeefdd2bd043303e57c669
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-10-29 22:54:10 +0500

    Changes

--- Frontend/src/components/useSessionCheck.jsx
+++ Frontend/src/components/useSessionCheck.jsx
@@ -41,3 +41,41 @@
 // };
 
 // export default useSessionCheck;
+
+
+
+
+
+
+// // pages/dashboard.js  , this is page code and make in pages.
+
+// import { useEffect } from 'react';
+// import useSessionCheck from "@/components/useSessionCheck";
+// import { useRouter } from 'next/router';
+
+// const Dashboard = () => {
+//     const sessionStatus = useSessionCheck();
+//     const router = useRouter();
+
+//     useEffect(() => {
+//         if (sessionStatus && sessionStatus.active === false) {
+//             router.push('/login');
+//         }
+//     }, [sessionStatus, router]);
+
+//     if (sessionStatus === null) {
+//         return <p>Loading...</p>;
+//     }
+
+//     return (
+//         <div>
+//             {sessionStatus.active ? (
+//                 <p>Welcome back, {sessionStatus.username}!</p>
+//             ) : (
+//                 <p>Redirecting to login...</p>
+//             )}
+//         </div>
+//     );
+// };
+
+// export default Dashboard;
